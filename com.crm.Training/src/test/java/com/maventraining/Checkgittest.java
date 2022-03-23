8e6dd87302bd8e91930fad321a0d7b77e1afe74e
DELL <DELL@DESKTOP-MHEBNHM>
2022-03-23 17:26:48 +0530

    push to master

---
+++ com.crm.Training/src/test/java/com/maventraining/Checkgittest.java
@@ -0,0 +1,21 @@
+package com.maventraining;
+
+import org.testng.annotations.Test;
+
+public class Checkgittest {
+	@Test
+	public void learn() {
+		System.out.println("maven learning");
+	}
+	@Test
+	public void Read() {
+		System.out.println("maven reading");
+	}
+
+	@Test
+	public void Write() {
+		System.out.println("maven Writing");
+	}
+	}
+
+
